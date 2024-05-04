12bec92258f5ca69714f437d3f80cf3d1b7bc2d8
Onoja Victor <onoja.jsdev@gmail.com>
2024-05-04 15:52:13 -0500

    add/update methods

--- Profile/userApi.mjs
+++ Profile/userApi.mjs
@@ -1,7 +1,13 @@
 export class UserAPI {
   constructor() {
-    this.baseURL = "https://three.t-pen.org/v1/"
-    this.token = window.TPEN_USER
+    this.baseURL = "https://dev.t-pen.org/v1/"
+    this.token = this.localInfo().authorization
+    this.displayName = this.localInfo().nickname
+    // this.displayName = this.getDisplayName()
+  }
+
+  localInfo() {
+    return window.TPEN_USER
   }
 
   async getProfile() {
@@ -19,7 +25,7 @@ export class UserAPI {
     }
   }
 
-  async updateRecord() {
+  async updateRecord(data) {
     try {
       const response = await fetch(`${this.baseURL}/my/profile/update`, {
         method: "PUT",
@@ -50,5 +56,31 @@ export class UserAPI {
       console.error("Error fetching user projects:", error)
       throw error
     }
+  }
+
+  async getDisplayName() {
+    const userInfo = await this.getProfile()
+    return userInfo?.displayName ?? userInfo?.profile?.displayName
+  }
+
+  async addToPublicProfile(data) {
+    try {
+      const userRecord = await this.getProfile()
+      const publicInfo = {...userRecord?.profile, ...data}
+      const payload = {...userRecord, profile: publicInfo}
+      const response = await this.updateRecord(payload)
+      return response
+      // We can either manipulate the data this way and use the the same route to handle all updates or,
+      // we can create a new route in Services and move these manipuations there.
+      // A third option would be to add a tag in the payload or via query strings
+    } catch (error) {
+      console.error("Error updating user record:", error)
+      throw error
+    }
+  }
+
+  async updatePrivateInformation(data) {
+    const response = await this.updateRecord(data)
+    return response
   }
 }
