16bcdf5676468c6482cf28028bb3b83a656536ad
Onoja Victor <onoja.jsdev@gmail.com>
2024-06-10 16:39:12 -0500

    add manage projects component

---
+++ manage/index.mjs
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("header button")
+  const content = document.getElementById("content")
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", async () => {
+      buttons.forEach(btn => btn.classList.remove('active'));
+      
+      button.classList.add('active');
+      const componentName = button.id
+      const response = await fetch(`${componentName}.html`)
+      const componentHtml = await response.text()
+      content.innerHTML = componentHtml
+    })
+  })
+
+  document.getElementById("projects").click()
+})
