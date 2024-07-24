8987e7bc8b7684f7f9b6823f59eda630152d666b
cubap <cubap@slu.edu>
2024-07-24 10:58:04 -0500

    Update index.js

--- components/default-transcribe/index.js
+++ components/default-transcribe/index.js
@@ -1,19 +1,33 @@
 // custom element named 'tpen-transcription' with a custom template built from the querystring 'projectID' parameter
 import { fetchProject, userMessage } from "../iiif-tools/index.mjs"
+import "https://cdn.jsdelivr.net/npm/manifesto.js"
+import "../line-image/index.js"
+import "../line-text/index.js"
 
 class TpenTranscriptionElement extends HTMLElement {
+    #projectID = new URLSearchParams(window.location.search).get('projectID')
+
     constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
+        super()
+        this.attachShadow({ mode: 'open' })
+        const transcriptionContainer = document.createElement('div')
+        transcriptionContainer.setAttribute('id', 'transcriptionContainer')
+        const imgTop = document.createElement('line-image')
+        imgTop.setAttribute('id', 'imgTop')
+        imgTop.setAttribute('projectID', this.#projectID)
+        const text = document.createElement('line-text')
+        text.setAttribute('id', 'text')
+        transcriptionContainer.append(imgTop, text)
+        
+        this.shadowRoot.append(transcriptionContainer)
     }
 
     connectedCallback() {
-        const projectID = new URLSearchParams(window.location.search).get('projectID');
-        if (!projectID) {
-            userMessage('No project ID provided');
+        if (!this.#projectID) {
+            userMessage('No project ID provided')
             return
         }
-        fetchProject(projectID);
+        return fetchProject(this.#projectID);
     }
 }
 
