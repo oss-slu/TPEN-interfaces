4370ee2d7924e7e841e3de9e868a13fd83916ff3
Onoja <onoja.jsdev@gmail.com>
2024-09-11 09:11:51 -0500

    Refactor permision-based visibility of elements

--- collaborators/index.mjs
+++ collaborators/index.mjs
@@ -2,10 +2,8 @@ import Project from "../api/project.mjs"
 
 let groupTitle = document.querySelector(".project-title")
 let groupMembersElement = document.querySelector(".group-members")
-const inviteSectionContainer = document.getElementById("invite-section-container")
-let submitButton = document.getElementById("submit")
-// let   removeButton = document.getElementById("remove-btn")
-let userEmail = document.getElementById("invitee-email")
+ let submitButton = document.getElementById("submit")
+ let userEmail = document.getElementById("invitee-email")
 
 const inviteForm = document.getElementById("invite-form")
 let errorHTML = document.getElementById("errorHTML")
@@ -18,26 +16,19 @@ let project = new Project(projectID)
 
 
 
-
-const TPEN_USER = window.TPEN_USER
-let token = TPEN_USER?.authorization ?? "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik9FVTBORFk0T1RVNVJrRXlOREl5TTBFMU1FVXdNMFUyT0RGQk9UaEZSa1JDTXpnek1FSTRNdyJ9.eyJodHRwOi8vc3RvcmUucmVydW0uaW8vYWdlbnQiOiJodHRwczovL3N0b3JlLnJlcnVtLmlvL3YxL2lkLzY1Zjg2MTVlYzQzYmQ2NjU2OGM2NjZmYSIsImh0dHA6Ly9yZXJ1bS5pby9hcHBfZmxhZyI6WyJ0cGVuIiwiZGxhIl0sImh0dHA6Ly9kdW5iYXIucmVydW0uaW8vYXBwX2ZsYWciOlsidHBlbiIsImRsYSJdLCJodHRwOi8vcmVydW0uaW8vdXNlcl9yb2xlcyI6eyJyb2xlcyI6WyJkdW5iYXJfdXNlcl9wdWJsaWMiLCJnbG9zc2luZ191c2VyX3B1YmxpYyIsImxyZGFfdXNlcl9wdWJsaWMiLCJyZXJ1bV91c2VyX3B1YmxpYyIsInRwZW5fdXNlcl9hZG1pbiIsInRwZW5fdXNlcl9pbmFjdGl2ZSJdfSwiaHR0cDovL2R1bmJhci5yZXJ1bS5pby91c2VyX3JvbGVzIjp7InJvbGVzIjpbImR1bmJhcl91c2VyX3B1YmxpYyIsImdsb3NzaW5nX3VzZXJfcHVibGljIiwibHJkYV91c2VyX3B1YmxpYyIsInJlcnVtX3VzZXJfcHVibGljIiwidHBlbl91c2VyX2FkbWluIiwidHBlbl91c2VyX2luYWN0aXZlIl19LCJpc3MiOiJodHRwczovL2N1YmFwLmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw2NWY4NjE1ZDZjNmJlYjIzMTVjZWY4MjIiLCJhdWQiOlsiaHR0cHM6Ly9jdWJhcC5hdXRoMC5jb20vYXBpL3YyLyIsImh0dHBzOi8vY3ViYXAuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTcyNTkxMzgwNiwiZXhwIjoxNzI1OTIxMDA2LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIHVwZGF0ZTpjdXJyZW50X3VzZXJfbWV0YWRhdGEgb2ZmbGluZV9hY2Nlc3MiLCJhenAiOiJiQnVnRk1XSFVvMU9oblNaTXBZVVh4aTNZMVVKSTdLbCJ9.u9MUgmCcZZUSN1TOF-Y6ECFr43CvYgE104YHdjLOjH1Jh2PcC07sOA3NhGDCdiP_BTC3WWF28dGdMmAjTZW7TCmylPrFN_KI1WQUqKi_0I1mkfq1iYbx5dKWvW6kOFnD4XC8-dWJci_cVN_86RhHOBjDVghbsRMF6plNbBPJKaTERx4ROU1DWGs0TnZpPTK9qBIuG_kMB-_FxLRyj8CRNF7pEQaqUuHj1lpFefPDWBSMr3wMJ6mogeG8HCjp-ole9uXEa55cg0LuKNzjeGONZZdmKnDkfL2Z9to0Jpcbhf2w2FDXOdI_q5MZVW-Te0ryhG3zcUNzkUWpUiz4YmjGAQ"
-
 userEmail.addEventListener("input", () => {
     errorHTML.innerHTML = ""
 })
-
-
 
 
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         const projectData = await project.loadData()
         renderProjectContributors(projectData)
+       
     } catch (error) {
         return errorHTML.innerHTML = error.status == 401 ? "Unauthorized request. Please log in to view this resource" : `Error ${error.status ?? ""}: ${error.statusText ?? error.toString()}`
     }
-
-    inviteSectionContainer.style.display = isOwnerOrLeader ? "block" : "none"
 
     removeMember()
 
@@ -85,7 +76,7 @@ function renderRoles(roles) {
     } else if (roles.includes("LEADER")) {
         return "leader"
     } else {
-        return roles.join(", ").toLowerCase()
+        return roles.join(", ").toLowerCase().replaceAll("_"," ")
     }
 }
 
@@ -105,13 +96,13 @@ function renderProjectContributors(project) {
                 isOwnerOrLeader = true
             };
 
-
             const memberHTML = `
                 <li class="member" data-member-id=${contributorId}> 
                   ${memberData.displayName ? `<span class="role">${renderRoles(memberData.roles)}</span>` : `<span class="pending">Pending</span>`}
                   ${memberData.displayName || memberData.email}
-
-                  ${isOwnerOrLeader ? `<button class="remove-button" id="remove-btn" data-member-id=${contributorId} data-member-name=${memberData.displayName || memberData.email} >Remove</button>` : ''}
+ 
+                 <button class="remove-button allow-invite is-hidden" id="remove-btn" data-member-id=${contributorId} data-member-name=${memberData.displayName || memberData.email} >Remove</button>
+ 
                 </li>
               `
 
@@ -123,6 +114,8 @@ function renderProjectContributors(project) {
         }
 
     }
+
+    setPermissionBasedVisibility()
 
 }
 
@@ -160,3 +153,14 @@ async function removeMember() {
     })
 }
 
+function setPermissionBasedVisibility() {
+    const inviteElements = document.querySelectorAll('.allow-invite')
+
+    inviteElements.forEach(element => {
+        if (isOwnerOrLeader) {
+            element.classList.remove('is-hidden')
+        } else {
+            element.classList.add('is-hidden')
+        }
+    })
+}
