b1207fc9c2d092a722a5a832441a9dab3e928c41
Patrick Cuba <cubap@slu.edu>
2024-11-08 15:45:42 -0600

    out of date local

--- api/User.mjs
+++ api/User.mjs
@@ -1,13 +1,14 @@
+import { eventDispatcher } from "../TPEN/events.mjs"
 /** Description: to use this class, initialize new class, set authentication token, then call required methods
- * @deprecated: use and update the file in /api instead
+ * 
  */
-export class User {
+export default class User {
   #authentication
-  constructor(userId) {
-    this.userId = userId
-    // this.baseURL = "https://dev.api.t-pen.org" 
-    this.baseURL = "http://localhost:3009" 
-    if (this.#authentication || this.userId) this.getProfile()
+  baseURL = "https://dev.api.t-pen.org"
+
+  constructor(_id) {
+    this._id = _id
+    // if (this.#authentication || this._id) this.getProfile()
   }
 
   /**
@@ -15,32 +16,36 @@ export class User {
    */
   set authentication(token) {
     let isNewToken = false;
-    if(token != this.#authentication){
+    if (token != this.#authentication) {
       isNewToken = true
-    } 
+    }
     this.#authentication = token
     if (isNewToken) this.getProfile()
   }
 
   async getProfile() {
-    if (!this.#authentication && !this.userId)
+    if (!this.#authentication && !this._id)
       throw Error("User ID is required")
 
-    const serviceAPI = `${this.baseURL}/${
-      this.#authentication ? "my/profile" : `user/:${this.userId}`
-    }`
+    const serviceAPI = `${this.baseURL}/${this.#authentication ? "my/profile" : `user/:${this._id}`
+      }`
     const headers = this.#authentication
-      ? new Headers({Authorization: `Bearer ${this.#authentication}`})
+      ? new Headers({ Authorization: `Bearer ${this.#authentication}` })
       : new Headers()
-    fetch(serviceAPI, {headers})
+    await fetch(serviceAPI, { headers })
       .then((response) => {
         if (!response.ok) Promise.reject(response)
-        const data = response.json()
-        // Object.assign(this, data) //
-
-        // the public user object has no display_name tag, it has a nme instead, hence the check below
-        this.display_name = this.#authentication?data.display_name:data.name
+        return response.json()
       })
+      .then((data) => {
+        Object.assign(this, data)
+        // the public user object has no displayName tag, it has a name instead, hence the check below
+        this.displayName = data.profile.displayName ?? data.name ?? "Anonymous"
+        if (data._sub) {
+          eventDispatcher.dispatch("tpen-user-loaded", this)
+        }
+      })
+    return this
   }
 
   async getProjects() {
@@ -48,7 +53,7 @@ export class User {
       Authorization: `Bearer ${this.#authentication}`
     })
 
-    return fetch(`${this.baseURL}/my/projects`, {headers})
+    return fetch(`${this.baseURL}/my/projects`, { headers })
       .then((response) => {
         if (!response.ok) {
           return Promise.reject(response)
@@ -101,7 +106,7 @@ export class User {
       .catch((error) => {
         const errorTemplate = `
           <li>
-            Error ${error.status??500}: ${error.statusText??"Unknown Server error"}
+            Error ${error.status ?? 500}: ${error.statusText ?? "Unknown Server error"}
           </li>
         `
         projectsList.insertAdjacentHTML("beforeend", errorTemplate)
@@ -130,8 +135,8 @@ export class User {
   async addToPublicProfile(data) {
     try {
       const userRecord = await this.getProfile()
-      const publicInfo = {...userRecord?.profile, ...data}
-      const payload = {...userRecord, profile: publicInfo}
+      const publicInfo = { ...userRecord?.profile, ...data }
+      const payload = { ...userRecord, profile: publicInfo }
       const response = await this.updateRecord(payload)
       return response
       // We can either manipulate the data this way and use the the same route to handle all updates or,
