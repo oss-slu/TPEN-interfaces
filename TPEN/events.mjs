2a2fecede055e533a0f1541ea876610c2d96f122
Patrick Cuba <cubap@slu.edu>
2024-11-08 15:49:14 -0600

    migrate TPEN modules

--- TPEN/events.mjs
+++ TPEN/events.mjs
@@ -1,23 +1,6 @@
-class EventDispatcher extends EventTarget {
-    constructor() {
-        super()
-    }
+/**
+ * @deprecated: use and update the file in /api instead
+ */
 
-    // Method to add an event listener
-    on(event, listener) {
-        this.addEventListener(event, listener)
-    }
-
-    // Method to remove an event listener
-    off(event, listener) {
-        this.removeEventListener(event, listener)
-    }
-
-    // Method to dispatch an event
-    dispatch(event, detail = {}) {
-        this.dispatchEvent(new CustomEvent(event, { detail }))
-    }
-}
-
-// Export a shared instance of EventDispatcher
-export const eventDispatcher = new EventDispatcher()
+import { eventDispatcher } from './api/events.mjs'
+export default eventDispatcher
---
+++ api/events.mjs
@@ -0,0 +1,23 @@
+class EventDispatcher extends EventTarget {
+    constructor() {
+        super()
+    }
+
+    // Method to add an event listener
+    on(event, listener) {
+        this.addEventListener(event, listener)
+    }
+
+    // Method to remove an event listener
+    off(event, listener) {
+        this.removeEventListener(event, listener)
+    }
+
+    // Method to dispatch an event
+    dispatch(event, detail = {}) {
+        this.dispatchEvent(new CustomEvent(event, { detail }))
+    }
+}
+
+// Export a shared instance of EventDispatcher
+export const eventDispatcher = new EventDispatcher()
