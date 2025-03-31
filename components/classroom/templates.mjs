8fd3b0a1e075074b366606b5ae8d6f554f47562e
Matthew Clendenning <124844665+Matthew-Clendenning@users.noreply.github.com>
2025-03-31 11:21:23 -0500

    fixed template loading issue

--- components/classroom/templates.mjs
+++ components/classroom/templates.mjs
@@ -1,7 +1,5 @@
-import { invitationTemplate } from './templates/invitationTemplate.mjs';
 import { rosterTemplate } from './templates/rosterTemplate.mjs';
 
 export const templates = [
-  invitationTemplate,
   rosterTemplate
 ];
