f53a38967d9b4fda4c4864439cc3f2729fb06815
cubap <cubap@slu.edu>
2025-04-22 12:57:22 -0500

    stubs and moving files around

--- components/gui/site/Header.js
+++ components/gui/site/Header.js
@@ -177,8 +177,8 @@ class TpenHeader extends HTMLElement {
                 </tpen-action-link>
                 <nav>
                     <ul>
-                        <li><a href="/home">Home</a></li>
-                        <li><a href="#">About</a></li>
+                        <li><a href="/index">Home</a></li>
+                        <li><a href="/about">About</a></li>
                         <li><a href="https://three.t-pen.org/logout?returnTo=${location}">Logout</a></li>
                     </ul>
                 </nav>
